db7ae96b8c9332f8eba995766c9f42b75096bf2c
Farid Sayyed <faridsayyed1010@gmail.com>
2023-04-10 09:58:07 +0000

    Update 4 files

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,29 @@
 import React from "react";
+import "../src/App.css";
 function Home() {
   return (
     <>
       <section id="header" className="">
         <div className="container-fluid nav bg">
-          <div className="row">
+          <div className="row h-50">
             <div className="col-10 mx-auto">
               <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                <h1>Grow your bussiness with<strong>Navbar</strong></h1>
+                <h1>
+                  Grow your bussiness with<strong>Navbar</strong>
+                </h1>
                 <h2 className="my-3">
                   we have team of tailent developer making websites
                 </h2>
                 <div className="mt-3-3">
-                  <a href="" className="btn-get-started">Get Started</a>
+                  <a href="/" className="btn-get-started">
+                    Get Started
+                  </a>
                 </div>
               </div>
             </div>
-          </div></div>
-
-
-      </section >
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,58 +1,79 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../src/App.css";
 const Navbar = () => {
   return (
     <>
       <div className="container-fluid nav bg">
-        <div className="row">
-          <div className="col-10 mx-auto">
+        <div className="row flex-grow-1">
+          <div className="col-26 mx-auto">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
               <div class="container-fluid">
-                < NavLink class="navbar-brand" to="/">Navbar</NavLink>
+                <NavLink class="navbar-brand" to="/">
+                  LOGO
+                </NavLink>
 
-                <button className="navbar-toggler"
+                <button
+                  className="navbar-toggler"
                   type="button"
                   data-bs-toggle="collapse"
                   data-bs-target="#navbarSupportedContent"
                   aria-controls="navbarSupportedContent"
                   aria-expanded="false"
-                  aria-label="Toggle navigation">
-
+                  aria-label="Toggle navigation"
+                >
                   <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div
                   className="collapse navbar-collapse"
-                  id="navbarSupportedContent">
+                  id="navbarSupportedContent"
+                >
+                  <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+                    {/* <li className="nav-item">
+                      <NavLink
+                        active
+                        ClassName="menu-active"
+                        exact
+                        class="nav-link active"
+                        aria-current="page"
+                        to="/"
+                      >
+                        Home
+                      </NavLink>
+                    </li> */}
 
-                  <ul className="navbar-nav ml-auto mb-2 mb-lg-0" />
-                  <li className="nav-item">
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/">
+                        Home
+                      </NavLink>
+                    </li>
 
-                    < NavLink active ClassName="menu-active"
-                      exact class="nav-link active"
-                      aria-current="page" to="/">Home</NavLink>
-                  </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/Service">
+                        Service
+                      </NavLink>
+                    </li>
 
-                  <li className="nav-item">
-                    < NavLink className="nav-link" to="/Service">Service</NavLink>
-                  </li>
+                    <li class="nav-item">
+                      <NavLink className="nav-link" to="/About">
+                        About
+                      </NavLink>
+                    </li>
 
-                  <li class="nav-item">
-                    < NavLink className="nav-link" to="/About">About</NavLink>
-                  </li>
-
-                  <li class="nav-item">
-                    < NavLink className="nav-link" to="/Contact">Contact</NavLink>
-                  </li>
-
+                    <li class="nav-item">
+                      <NavLink className="nav-link" to="/Contact">
+                        Contact
+                      </NavLink>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </nav>
           </div>
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 export default Navbar;
